176616c077f1b7d8e5992bc62209ce64a3525dfd
toppms <top1604@naver.com>
2021-02-05 21:12:45 +0900

    async & await

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 import PropTypes from  "prop-types";
+import axios from "axios";
 
 class App extends React.Component {
     state = {
       isLoading: true,
       movies: {}
     };
+    getMovies = async() => {
+      const movie = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    }
     componentDidMount() {
-      setTimeout(() => {
-        this.setState({isLoading: false});
-      }, 6000);
+      this.getMovies();
     }
     render() {
       const {isLoading} = this.state;
